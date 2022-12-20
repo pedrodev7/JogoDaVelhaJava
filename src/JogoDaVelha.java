80ab2eb8da916bbb4551984805360782f99b385f
pedrodev7 <36186318+pedrodev7@users.noreply.github.com>
2022-12-20 00:13:00 -0300

    Definindo atributos e construtor da classe JogoDaVelha

--- src/JogoDaVelha.java
+++ src/JogoDaVelha.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
+
 public class JogoDaVelha {
 
+private Character[][] tabuleiro;
+private int rodada;
+private int vez;
+private Scanner sc;
+
+public JogoDaVelha() {
+  tabuleiro = new Character[3][3];
+  rodada = 0;
+  vez = 0;
+  }
 
 } 
