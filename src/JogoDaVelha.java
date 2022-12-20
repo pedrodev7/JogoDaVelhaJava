3ce4db2871991fe53a3e62394189560f45a3166c
pedrodev7 <36186318+pedrodev7@users.noreply.github.com>
2022-12-20 00:19:20 -0300

    Implementando método para imprimir tabuleiro

--- src/JogoDaVelha.java
+++ src/JogoDaVelha.java
@@ -11,6 +11,19 @@ public JogoDaVelha() {
   tabuleiro = new Character[3][3];
   rodada = 0;
   vez = 0;
+}
+
+private void imprimirTabuleiro() {
+  for (int i = 0; i < tabuleiro.length; i++) {
+    for (int j = 0; j < tabuleiro.length; j++) {
+      if (tabuleiro[i][j] == null) {
+        System.out.print("_ ");
+      } else {
+        System.out.print(tabuleiro[i][j] + " ");
+      }
+    }
+    System.out.println();
   }
+}
 
 } 
